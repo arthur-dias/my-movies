2ffcb1e07225114e48c1a12f597a381ee06e76e3
Arthur Dias <dias.arthurr@gmail.com>
2023-11-06 14:42:33 -0300

    Utils changes

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,4 +1,11 @@
-export default function getRandomNumber(maxNumber: number): number {
+export function getRandomNumber(maxNumber: number): number {
   const randomNumber = Math.floor(Math.random() * maxNumber)
   return randomNumber
 }
+
+export function formattedRating(voteAverage: number): string {
+  const rating = voteAverage.toFixed(1)
+  return rating
+}
+
+export default { getRandomNumber, formattedRating }
