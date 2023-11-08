e7b274519321eccb697dad78122e5347518eae21
Arthur Dias <dias.arthurr@gmail.com>
2023-11-08 18:02:03 -0300

    Created Footer

--- src/App.vue
+++ src/App.vue
@@ -3,11 +3,13 @@
     <MainNav />
   </header>
   <RouterView />
+  <Footer />
 </template>
 
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
 import MainNav from './components/MainNav.vue'
+import Footer from './components/Footer.vue'
 </script>
 
 <style scoped></style>
---
+++ src/components/Footer.vue
@@ -0,0 +1,25 @@
+<template>
+  <footer>
+    <p>Feito por <a href="https://github.com/arthur-dias/">Arthur</a> | 2023</p>
+  </footer>
+</template>
+
+<style scoped>
+footer {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 2rem 0;
+  background-color: #1f1c2b;
+  margin-top: 5rem;
+}
+p {
+  font-weight: 800;
+  color: #fffffe;
+}
+
+p > a {
+  text-decoration: none;
+  color: #ff8906;
+}
+</style>
